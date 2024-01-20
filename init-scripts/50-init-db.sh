cc77984e6ecb093be84dbfd3f101b114cd9a5f25
bikboktech <bikboktech@gmail.com>
2024-01-20 16:16:46 +0000

    chore: update init-scripts

--- init-scripts/50-init-db.sh
+++ init-scripts/50-init-db.sh
@@ -1,7 +1,7 @@
 ALLOWED_HOST="%"
 
-find "musicology-deploy/init-scripts/" -follow -type f -name "*.sql" | sort -V | while read -r f; do
+find "/docker-entrypoint-initdb.d/" -follow -type f -name "*.sql" | sort -V | while read -r f; do
     SQL_CONTENT=$(cat $f | sed "s|{MYSQL_USER}|${MYSQL_USER}|g")
     SQL_CONTENT=$(echo $SQL_CONTENT | sed "s|{ALLOWED_HOST}|${ALLOWED_HOST}|g")
-    mysql --user $MYSQL_USER --password $MYSQL_ROOT_PASSWORD --execute $SQL_CONTENT
+    mysql --user $MYSQL_USER -p --execute "$SQL_CONTENT"
 done
--- init-scripts/user.sql
+++
@@ -1,4 +0,0 @@
-UPDATE mysql.user SET host='{ALLOWED_HOST}' WHERE host='localhost' AND user='{MYSQL_USER}';
-UPDATE mysql.db SET host='{ALLOWED_HOST}' WHERE host='localhost' AND user='{MYSQL_USER}';
-FLUSH PRIVILEGES;
-COMMIT;
